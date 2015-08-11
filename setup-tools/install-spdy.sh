f4de5956e9c8c5dfcfe300f8bdf00edc65568cc2
Paul Lan <slan@tibco-support.com>
2015-08-12 01:34:03 +0800

    Make SPDY key/cert input faster

--- setup-tools/install-spdy.sh
+++ setup-tools/install-spdy.sh
@@ -5,46 +5,18 @@ source ~/.global-utils.sh
 echoS "Please input the file (with path) of your SSL Key file  (*.key) : \n\n(You could not use self-signed SSL cert. You could get \
 a free copy from https://www.startssl.com/)\n"
 
-maxTryKey=3
-while [ ${maxTryKey} -gt 0 ]; do
-  read -p "Input *.key File Path (e.g. /root/mydomain.com.key):  " key
-  key=$(removeWhiteSpace "${key}")
 
-  if [[ ! -f ${key} ]]; then
-    echoS "The SSL Key file ${key} is not existed. Retry."
-  else
-    break
-  fi
-  ((maxTryKey--))
+key=$(getUserInput "Input *.key File Path (e.g. /root/mydomain.com.key):   " file 1)
+echoS "Selected key file is ${key}"
 
-done
-
-
-
-
-echoS "Please input the file (with path) of your SSL Cert file  (*.crt) : \n\n"
-
-
-maxTryCert=3
-while [ ${maxTryCert} -gt 0 ]; do
-  read -p "Input *.crt File Path (e.g. /root/mydomain.com.crt): " cert
-
-  cert=$(removeWhiteSpace "${cert}")
-
-  if [[ ! -f ${cert} ]]; then
-    echoS "The SSL cert file ${cert} is not existed. Retry"
-  else
-    break
-  fi
-  ((maxTryCert--))
-done
+cert=$(getUserInput "input the file (with path) of your SSL Cert file  (*.crt) :  " file 1)
+echoS "Selected cert file is ${cert}"
 
 if [[  -f ${key} &&  -f ${cert} ]]; then
   echoS "Copy Key ${key} and Cert ${cert} to ${configDir}"
   cp ${key} ${SPDYSSLKeyFile}
   cp ${cert} ${SPDYSSLCertFile}
 fi
-
 
 sudo npm install -g spdyproxy > /dev/null 2>&1
 
