ca1014190a8bf55e15fc525bc135a7edf28b2609
Paul Shunfang Lan <paul.lan@oracle.com>
2015-11-19 20:26:19 +0800

    [Enhancement] change forever restart testing-web daily

--- bin/restart-misc.sh
+++ bin/restart-misc.sh
@@ -2,5 +2,5 @@
 
 source /opt/.global-utils.sh
 
+forever stop ${freeServerRootMisc}/testing-web.js
 forever start ${freeServerRootMisc}/testing-web.js
-forever restart ${freeServerRootMisc}/testing-web.js
--- misc/testing-web.js
+++ misc/testing-web.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Free Server is online :) \n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1 style="color: #4488ff;">Free Server is online :)</h1> \n');
 }).listen(80, '0.0.0.0');
 console.log('Server running at http://localhost/');
