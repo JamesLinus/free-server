7296e712696208cd13a0961e73df91d8d917e73c
Paul Lan <slan@tibco-support.com>
2015-12-15 00:51:54 +0800

    [BUG] restart script may generate stderr if process is not running. So it should be tolerant.

--- setup-tools/init.sh
+++ setup-tools/init.sh
@@ -23,21 +23,18 @@ exitOnError "${catchError}"
 
 echoS "Restart Shadowsocks/SPDY/IPSec"
 
-catchError=$(${freeServerRoot}/restart-ipsec  2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
+${freeServerRoot}/restart-ipsec
 
-catchError=$(${freeServerRoot}/restart-shadowsocks 2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
+${freeServerRoot}/restart-shadowsocks
 
 #${freeServerRoot}/restart-spdy
-catchError=$(${freeServerRoot}/restart-spdy-nghttpx-squid 2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
+${freeServerRoot}/restart-spdy-nghttpx-squid
 
 echoS "Create a simple website for testing purpose."
 
 catchError=$(npm install -g forever 2>&1 >> ${loggerStdoutFile})
 exitOnError "${catchError}"
 
-catchError=$(${utilDir}/restart-misc.sh  2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
+${utilDir}/restart-misc.sh
 
+#EOF
