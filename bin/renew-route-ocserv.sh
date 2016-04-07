108efb9677fa7d2dcb6b801bc785fc312850174d
Paul Shunfang Lan <paul.lan@oracle.com>
2016-04-07 09:46:13 +0800

    [enhancement] add auto-renew Routing Table of Cisco AnyConnect VPN, ocserv

---
+++ bin/renew-route-ocserv.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source /opt/.global-utils.sh
+
+if [ "x$1" = "x-h" -o "x$1" = "x--help" ]
+then
+  echo "Usage: $0"
+
+  exit 0
+fi
+
+if [[ ! -f ${letsEncryptKeyPath} ]]; then
+  echoS "The SSL Key file ${key} is not existed. Exit" "stderr"
+  exit 1
+fi
+
+
+if [[ ! -f ${letsEncryptCertPath} ]]; then
+  echoS "The SSL cert file ${cert} is not existed. Exit" "stderr"
+  exit 1
+fi
+
+if [[ ! -f ${ocservConfig} ]]; then
+    echoS "Ocserv config file (${ocservConfig}) is not detected. This you may not install it correctly. Exit." "stderr"
+    exit 1
+fi
+
+if [[ ! -f ${ocservPasswd} ]]; then
+    echoS "Ocserv Password file (${ocservPasswd}) is not detected. This you may not install it correctly. Exit." "stderr"
+    exit 1
+fi
+
+# set iptable to connecto Internet
+enableIptableToConnectInternet
+
+# kill and start
+pkill -ef ^ocserv
+sleep 2
+
+${freeServerRoot}/restart-dead-ocserv
+
+echoS "Restarted ocserv"
+sleep 1
+
+ps aux |  grep ocserv
