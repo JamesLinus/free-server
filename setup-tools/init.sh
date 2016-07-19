f263af65cb6c1865b553f776644c1e9aaf401a98
Paul Shunfang Lan <paul.lan@oracle.com>
2016-07-19 20:54:44 +0800

    [bug] fix bug on Digital Ocean Ubuntu 15, cron.service restarted failed

--- setup-tools/init.sh
+++ setup-tools/init.sh
@@ -9,28 +9,22 @@ echoS "Write to crontab for auto restart"
 /bin/bash ${utilDir}/cron-reboot-daily-generate-cron.d.sh  2>&1 >> ${loggerStdoutFile}
 
 catchError=$(/bin/bash ${utilDir}/cron-renew-letsencrypt.sh  2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
 
 # smart service watcher for every 2 minutes
 catchError=$(/bin/bash ${utilDir}/cron-shadowsocks-forever-process-running-generate-cron.d.sh  2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
 
 #catchError=$(${freeServerRoot}/cron-ipsec-forever-process-running-generate-cron.d 2>&1 >> ${loggerStdoutFile})
 catchError=$(/bin/bash ${utilDir}/cron-ocserv-forever-process-running-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
 
 catchError=$(/bin/bash ${utilDir}/cron-ocserv-renew-route-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
 
 #${freeServerRoot}/cron-spdy-forever-process-running-generate-cron.d
 catchError=$(/bin/bash ${utilDir}/cron-spdy-nghttpx-squid-forever-process-running-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
 
 /bin/bash ${utilDir}/cron-misc-forever-process-running-generate-cron.d.sh
 
 # restart cron service
 catchError=$(service cron restart 2>&1 >> ${loggerStdoutFile})
-exitOnError "${catchError}"
 
 #echoS "Restart Shadowsocks/SPDY/IPSec"
 echoS "Restart Shadowsocks/SPDY/Cisco AnyConnect"
