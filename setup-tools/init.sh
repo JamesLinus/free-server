ed2ff0a8b39fdc7813b87c81267df98af7ff4240
Paul Shunfang Lan <paul.lan@oracle.com>
2016-08-24 23:32:36 +0800

    [bug] fix forever start failure

--- setup-tools/init.sh
+++ setup-tools/init.sh
@@ -5,22 +5,22 @@ source /opt/.global-utils.sh
 
 echoS "Write to crontab for auto restart"
 
-/bin/bash ${gitRepoFreeServerPath}/bin/cron-reboot-daily-generate-cron.d.sh  2>&1 >> ${loggerStdoutFile}
+/bin/bash ${binDir}/cron-reboot-daily-generate-cron.d.sh  2>&1 >> ${loggerStdoutFile}
 
-catchError=$(/bin/bash ${gitRepoFreeServerPath}/bin/cron-renew-letsencrypt.sh  2>&1 >> ${loggerStdoutFile})
+catchError=$(/bin/bash ${binDir}/cron-renew-letsencrypt.sh  2>&1 >> ${loggerStdoutFile})
 
 # smart service watcher for every 2 minutes
-catchError=$(/bin/bash ${gitRepoFreeServerPath}/bin/cron-shadowsocks-r-forever-process-running-generate-cron.d.sh  2>&1 >> ${loggerStdoutFile})
+catchError=$(/bin/bash ${binDir}/cron-shadowsocks-r-forever-process-running-generate-cron.d.sh  2>&1 >> ${loggerStdoutFile})
 
 #catchError=$(${freeServerRoot}/cron-ipsec-forever-process-running-generate-cron.d 2>&1 >> ${loggerStdoutFile})
-catchError=$(/bin/bash ${gitRepoFreeServerPath}/bin/cron-ocserv-forever-process-running-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
+catchError=$(/bin/bash ${binDir}/cron-ocserv-forever-process-running-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
 
-catchError=$(/bin/bash ${gitRepoFreeServerPath}/bin/cron-ocserv-renew-route-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
+catchError=$(/bin/bash ${binDir}/cron-ocserv-renew-route-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
 
 #${freeServerRoot}/cron-spdy-forever-process-running-generate-cron.d
-catchError=$(/bin/bash ${gitRepoFreeServerPath}/bin/cron-spdy-nghttpx-squid-forever-process-running-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
+catchError=$(/bin/bash ${binDir}/cron-spdy-nghttpx-squid-forever-process-running-generate-cron.d.sh 2>&1 >> ${loggerStdoutFile})
 
-/bin/bash ${gitRepoFreeServerPath}/bin/cron-misc-forever-process-running-generate-cron.d.sh
+/bin/bash ${binDir}/cron-misc-forever-process-running-generate-cron.d.sh
 
 # restart cron service
 catchError=$(service cron restart 2>&1 >> ${loggerStdoutFile})
@@ -38,8 +38,8 @@ ${binDir}/restart-spdy-nghttpx-squid.sh
 
 echoS "Create a simple website for testing purpose."
 
-npm install -g forever
+catchError=$(npm install -g forever 2>&1 >> ${loggerStdoutFile})
 
-${gitRepoFreeServerPath}/bin/restart-misc.sh
+${binDir}/restart-misc.sh
 
 #EOF
