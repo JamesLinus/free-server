a5f8a7d5e8f7ace7c6a9484b7766cd9d9222161e
Paul Lan <lanshunfang@Pauls-MacBook-Air.local>
2017-04-02 13:20:53 +0800

    [bug] update letsencrypt install

--- setup-tools/install-letsencrypt.sh
+++ setup-tools/install-letsencrypt.sh
@@ -26,7 +26,10 @@ installLetsencrypt() {
     echoS "Installing Let's Encrypt"
 #    git clone https://github.com/letsencrypt/letsencrypt ./ 2>&1 >> ${loggerStdoutFile}
 #    ./letsencrypt-auto --help --agree-tos 2>&1 >> ${loggerStdoutFile}
-    apt-get install -y letsencrypt  2>&1 >> ${loggerStdoutFile}
+  add-apt-repository -y ppa:certbot/certbot
+  apt-get -y update
+  #apt-get install -y letsencrypt  2>&1 >> ${loggerStdoutFile}
+  apt-get install -y certbot
 
 
 
@@ -35,12 +38,12 @@ installLetsencrypt() {
 
 getCert() {
 #    eval "$letsencryptCertBotPath certonly --standalone --non-interactive --agree-tos -n --email=${freeServerUserEmail} -d ${freeServerName}"
-    letsencrypt certonly --standalone --non-interactive --agree-tos -n --email=${freeServerUserEmail} -d ${freeServerName}
-    letsencrypt renew --dry-run --agree-tos
+    certbot certonly --standalone --non-interactive --agree-tos -n --email=${freeServerUserEmail} -d ${freeServerName}
+    certbot renew --dry-run --agree-tos
 }
 
 enableAutoRenew() {
-    echo "6 50 *  * 1 root ~/renew_letsencrypt.sh" > /etc/cron.d/renew_letsencrypt
+    echo "6 50 *  * 1 root certbot renew --agree-tos" > /etc/cron.d/renew_letsencrypt
     # run for first time
 }
 
